3f00354932ab82090a77fe85934979e864504b0c
Aavesh Kumar Sinha <aks20000@hotmail.com>
2019-12-10 10:51:40 +0530

    Create Day1Task3.py
    Reverse the input number

---
+++ Python/Day1Task3.py
@@ -0,0 +1,14 @@
+#Q. Input a number and reverse it
+Number = int(input("Please Enter any Number: "))    
+Reverse = 0    
+while(Number > 0):    
+    Reminder = Number %10    
+    Reverse = (Reverse *10) + Reminder    
+    Number = Number //10    
+     
+print("\n Reverse of entered number is = %d" %Reverse)
+'''
+Please Enter any Number: 321
+
+Reverse of entered number is = 123
+'''
